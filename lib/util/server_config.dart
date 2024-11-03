b7a7b75073154d91293cd6e6007c03468baea4ac
tw-yoo <twyoo99@gmail.com>
2024-11-03 16:46:56 +0900

    edit url

--- lib/util/server_config.dart
+++ lib/util/server_config.dart
@@ -1,1 +1,1 @@
-const String gptServerUrl = String.fromEnvironment('THEWELL_GPT_SERVER_URL', defaultValue: 'http://gpt.thewell-academy.com');
+const String gptServerUrl = String.fromEnvironment('THEWELL_GPT_SERVER_URL', defaultValue: 'http://thewell-gpt-lb-101888234.ap-northeast-2.elb.amazonaws.com');
